b79f9e349c93ca036b45fc4810f79f28621d92b3
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-29 18:48:30 +0200

    2 new projects

--- js/components/projects/projectsArray.mjs
+++ js/components/projects/projectsArray.mjs
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: "The Function",
-    description: "Coding blog project, with a unique navigation around the corners",
+    description: "Coding blog project, with a unique navigation around the corners, first year project exam. INFO: this website has currently no content since i have taken down the headless WP and hosting with one.com because of the cost, will be replaced with a new CMS",
     images: [
       `../../../assets/images/code-blogg.jpg`,
       `../../../assets/images/cards/theFunction/the-function-blog-posts.jpg`,
@@ -13,10 +13,14 @@ export const projects = [
       "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/HTML.svg",
       "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/CSS.svg",
     ],
+    links: {
+      hostedDemo: "https://thefunction-sigvel.netlify.app/",
+      repository: "https://github.com/Sigvel/Project-exam-1",
+    }
   },
   {
     title: "Community Science Museum",
-    description: "Interactive community science museum website",
+    description: "Interactive community science museum website, this is one of my first projects.",
     images: [
       `../../../assets/images/interactive-museum.jpg`,
       `../../../assets/images/cards/communityScience/community-science-museum.jpg`,
@@ -28,6 +32,10 @@ export const projects = [
       "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/HTML.svg",
       "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/CSS.svg",
     ],
+    links: {
+      hostedDemo: "https://gallant-bassi-4a5676.netlify.app/",
+      repository: "https://github.com/Sigvel/semester-project",
+    }
   },
   {
     title: "Social media app",
@@ -44,5 +52,51 @@ export const projects = [
       "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/HTML.svg",
       "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/CSS.svg",
     ],
+    links: {
+      hostedDemo: "https://the-social-media.netlify.app/src/pages/login/index.html",
+      repository: "https://github.com/Sigvel/TheSocial",
+    }
+  },
+  {
+    title: "Merrno E-Commerce",
+    description: "E-commerce website",
+    images: [
+      `../../../assets/images/cards/merrno/merrno-homepage.jpg`,
+      `../../../assets/images/cards/merrno/merrno-product.jpg`,
+      `../../../assets/images/cards/merrno/merrno-cart.jpg`,
+      `../../../assets/images/cards/merrno/merrno-contact.jpg`,
+    ],
+    tools: [
+      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/JavaScript.svg",
+      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/Sass.svg",
+      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/React-Dark.svg",
+      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/StyledComponents.svg",
+      "../../../assets/images/icons/zustand-icon.png",
+    ],
+    links: {
+      hostedDemo: "https://online-shop-merrno.netlify.app/",
+      repository: "https://github.com/Sigvel/Merrno",
+    }
+  },
+  {
+    title: "Holidaze",
+    description: "Vacation website, allows for users to rent out their venues and book a venue a user has created",
+    images: [
+      `../../../assets/images/cards/holidaze/holidaze-homepage.jpg`,
+      `../../../assets/images/cards/holidaze/holidaze-login.jpg`,
+      `../../../assets/images/cards/holidaze/holidaze-profile.jpg`,
+      `../../../assets/images/cards/holidaze/holidaze-user-profile.jpg`,
+      `../../../assets/images/cards/holidaze/holidaze-venue.jpg`,
+    ],
+    tools: [
+      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/JavaScript.svg",
+      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/TailwindCSS-Dark.svg",
+      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/Redux.svg",
+      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/React-Dark.svg",
+    ],
+    links: {
+      hostedDemo: "https://holidaze-site.netlify.app/",
+      repository: "https://github.com/Sigvel/project-exam-2",
+    }
   },
 ];
