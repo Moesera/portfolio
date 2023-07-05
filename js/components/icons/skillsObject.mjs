da41309b9c62354be752e4efe252988961281b15
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-07-05 21:18:46 +0200

    skills new folder

---
+++ js/components/icons/skillsObject.mjs
@@ -0,0 +1,78 @@
+export const skills = [
+      {
+        title: "CSS",
+        icon: "../../../assets/icons/CSS-png.png",
+      },
+      {
+        title: "HTML",
+        icon: "../../../assets/icons/HTML-png.png",
+      },
+      {
+        title: "JavaScript",
+        icon: "../../../assets/icons/JavaScript-png.png",
+      },
+      {
+        title: "ExpressJS",
+        icon: "../../../assets/icons/ExpressJS-Dark-png.png",
+      },
+      {
+        title: "NodeJS",
+        icon: "../../../assets/icons/NodeJS-Dark-png.png",
+      },
+      {
+        title: "NextJS",
+        icon: "../../../assets/icons/NextJS-Dark-png.png",
+      },
+      {
+        title: "React",
+        icon: "../../../assets/icons/React-Dark-png.png",
+      },
+      {
+        title: "Svelte",
+        icon: "../../../assets/icons/Svelte-png.png",
+      },
+      {
+        title: "Sass",
+        icon: "../../../assets/icons/Sass-png.png",
+      },
+      {
+        title: "Tailwind CSS",
+        icon: "../../../assets/icons/TailwindCSS-Dark-png.png",
+      },
+      {
+        title: "Git",
+        icon: "../../../assets/icons/Git-png.png",
+      },
+      {
+        title: "Github Actions",
+        icon: "../../../assets/icons/GithubActions-Dark-png.png",
+      },
+      {
+        title: "Photoshop",
+        icon:  "../../../assets/icons/Photoshop-png.png",
+      },
+      {
+        title: "PostgreSQL",
+        icon: "../../../assets/icons/PostgreSQL-Dark-png.png",
+      },
+      {
+        title: "Postman",
+        icon: "../../../assets/icons/Postman-png.png",
+      },
+      {
+        title: "Jest",
+        icon: "../../../assets/icons/Jest-png.png",
+      },
+      {
+        title: "Wordpress",
+        icon: "../../../assets/icons/Wordpress-png.png",
+      },
+      {
+        title: "Redux",
+        icon: "../../../assets/icons/Redux-png.png",
+      },
+      {
+        title: "Styled Components",
+        icon: "../../../assets/icons/StyledComponents-png.png",
+      },
+];
