7cc10ab1b0748ea8887a766f3468a7218a21674b
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-07-06 21:01:38 +0200

    added new icons to project cards

--- js/components/projects/projectsArray.mjs
+++ js/components/projects/projectsArray.mjs
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: "Social media app",
-    description: "Social media app, to test the website sign up with @noroff.stud.no email",
+    description: "Social media app, if you want to test the website, you have to sign with a (fake) @noroff.stud.no email. Instructions should be in the readme file of the project",
     images: [
       `../../../assets/images/cards/theSocial/social-homepage.jpg`,
       `../../../assets/images/cards/theSocial/social-login.jpg`,
@@ -10,10 +10,10 @@ export const projects = [
       `../../../assets/images/cards/theSocial/social-details.jpg`,
     ],
     tools: [
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/JavaScript.svg",
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/Sass.svg",
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/HTML.svg",
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/CSS.svg",
+      "../../../assets/icons/JavaScript-png.png",
+      "../../../assets/icons/Sass-png.png",
+      "../../../assets/icons/HTML-png.png",
+      "../../../assets/icons/CSS-png.png",
     ],
     links: {
       hostedDemo: "https://the-social-media.netlify.app/index.html",
@@ -22,7 +22,7 @@ export const projects = [
   },
   {
     title: "Merrno E-Commerce",
-    description: "E-commerce website",
+    description: "E-commerce website, the api used only had products available, the cart system is made with zustand and should work properly. If you want to report a bug please do so by submitting an issue since the contact form is not connected",
     images: [
       `../../../assets/images/cards/merrno/merrno-homepage.jpg`,
       `../../../assets/images/cards/merrno/merrno-product.jpg`,
@@ -30,11 +30,11 @@ export const projects = [
       `../../../assets/images/cards/merrno/merrno-contact.jpg`,
     ],
     tools: [
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/JavaScript.svg",
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/Sass.svg",
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/React-Dark.svg",
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/StyledComponents.svg",
-      "../../../assets/images/icons/zustand-icon.png",
+      "../../../assets/icons/JavaScript-png.png",
+      "../../../assets/icons/Sass-png.png",
+      "../../../assets/icons/React-Dark-png.png",
+      "../../../assets/icons/StyledComponents-png.png",
+      "../../../assets/icons/zustand-png.png",
     ],
     links: {
       hostedDemo: "https://sensational-naiad-7cfd07.netlify.app/",
@@ -43,7 +43,7 @@ export const projects = [
   },
   {
     title: "Holidaze",
-    description: "Vacation website, allows for users to rent out their venues and book a venue a user has created",
+    description: "Vacation website, allows for users to rent out their venues and book venues, you have to sign up with an (fake) @stud.noroff.no email because of api restrictions, you should find proper instructions in the readme for the project",
     images: [
       `../../../assets/images/cards/holidaze/holidaze-homepage.jpg`,
       `../../../assets/images/cards/holidaze/holidaze-login.jpg`,
@@ -52,14 +52,30 @@ export const projects = [
       `../../../assets/images/cards/holidaze/holidaze-venue.jpg`,
     ],
     tools: [
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/JavaScript.svg",
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/TailwindCSS-Dark.svg",
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/Redux.svg",
-      "https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/React-Dark.svg",
+      "../../../assets/icons/JavaScript-png.png",
+      "../../../assets/icons/TailwindCSS-Dark-png.png",
+      "../../../assets/icons/Redux-png.png",
+      "../../../assets/icons/React-Dark-png.png",
     ],
     links: {
       hostedDemo: "https://holidaze-site.netlify.app/",
       repository: "https://github.com/Sigvel/project-exam-2",
+    }
+  },
+  {
+    title: "Chatbot",
+    description: "Chatbot created with the openAi api, simply insert your key and prompt to the bot. the bot uses GPT-3.5-turbo. This is an open-source project, please feel free to clone it and build upon it. You can also contribute by submitting a pull request",
+    images: [
+      `../../../assets/images/cards/chatbot/chatbot-homepage.jpg`,
+    ],
+    tools: [
+      "../../../assets/icons/JavaScript-png.png",
+      "../../../assets/icons/CSS-png.png",
+      "../../../assets/icons/HTML-png.png",
+    ],
+    links: {
+      hostedDemo: "https://portfolio-panda-dev.netlify.app/",
+      repository: "https://github.com/Sigvel/chatbot",
     }
   },
 ];
